926c566172786881966247b4164bfba523030695
robonexx <robertwagar@gmail.com>
2022-12-30 13:16:07 +0100

    just setting the hamburger button to toggle the nav on click and show if it is open

--- hamburger.js
+++ hamburger.js
@@ -1,6 +1,9 @@
-let menuBtn = document.querySelector('#hamburger')
+let menuBtn = document.querySelector('#hamburger');
+let nav = document.querySelector('.nav');
 
 menuBtn.addEventListener('click', () => {
-    console.log('clicked button')
-    menuBtn.classList.toggle('active')
-})
+  console.log('clicked button');
+  menuBtn.classList.toggle('active');
+
+  nav.classList.toggle('open');
+});
